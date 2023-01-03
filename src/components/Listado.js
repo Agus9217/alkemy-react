42ece65dc35c24ef1d7a7105717335a1b5438932
Agus9217 <justinibarrola92@gmail.com>
2023-01-03 17:00:06 -0300

    alkemy-react

---
+++ src/components/Listado.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Listado = () => {
+
+  const token = localStorage.getItem('token')
+  console.log(token)
+
+  return (
+    <div>Listado</div>
+  )
+}
+
+export default Listado
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,55 @@
+import axios from 'axios'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
 
 const Login = () => {
+
+  const navigate = useNavigate()
+  const swAlert = withReactContent(Swal)
 
   const submitHandler = (e) => {
     e.preventDefault()
     const email = e.target.email.value
     const password = e.target.password.value
 
-    const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
 
     if(email === '' || password === '') {
-      console.log('Los campos no pueden estar vacios')
+      swAlert.fire(
+        <h2>Los campos no pueden estar vacios</h2>
+      )
       return
     }
 
     if(email !== '' && !regexEmail.test(email)) {
-      console.log('Debes escribir un correo valido')
+      swAlert.fire(
+        <h2>Debes escribir un correo valido</h2>
+      )
       return
     }
+
+    if(email !== 'challenge@alkemy.org' || password !== 'react') {
+      swAlert.fire(
+        <h2>credenciales invalidas</h2>
+      )
+      return
+    }
+
+
+    console.log('OK credenciales enviadas')
+    axios.post('http://challenge-react.alkemy.org', { email, password })
+    .then(res => {
+      swAlert.fire(
+        <h2>Login valido</h2>
+      )
+      const token = res.data.token
+      localStorage.setItem('token', token)
+      navigate('/listado')
+    })
   }
-
-
 
   return (
     <>
